46b6549ca9243f3a7e468c2a2cf16c57d2fd7077
Gustavo Coronel <gcoronelc@gmail.com>
2016-05-24 09:31:16 -0500

    ABD

--- ABD/Clase-24MAY2016.sql
+++ ABD/Clase-24MAY2016.sql
@@ -3,13 +3,15 @@ select @@ROWCOUNT, @@ERROR;
 go
 
 /*
+Ejercicio 01
 Desarrollar un procedimiento que
 permita actualizar la nota de un estudiantes
 en un curso.
 */
 
 
-select * from Matricula;
+select * from Matricula
+order by 1 desc;
 go
 
 alter procedure usp_act_nota
@@ -32,15 +34,65 @@ BEGIN
 		update Matricula set ExaParcial = @Nota,
 		Promedio = (@Nota + ExaFinal) / 2 
 		where IdCursoProg = @IdCursoProg and IdAlumno = @IdAlumno;
-		if(@@ROWCOUNT == 0)
+		if(@@ROWCOUNT = 0)
 			raiserror('Datos incorrectos.',16,1);
 		return;
 	end;
 END;
 GO
 
-exec usp_act_nota 4,'A0001',34,'EP';
+select * from Matricula order by 1 desc;
 go
+
+exec usp_act_nota 8024,'A0048',15,'EP';
+go
+
+
+/*
+Ejercicio 02
+Desarrollar un procedimiento que permita
+matricular a un estudiante.
+*/
+create procedure usp_matricula
+( @IdCursoProg int, @IdAlumno char(5) )
+as
+BEGIN
+	-- Variables
+	declare @Vacantes int;
+	-- Ver si hay vacantes
+	select @Vacantes = Vacantes 
+	from CursoProgramado where IdCursoProg = @IdCursoProg;
+	if( @@ROWCOUNT = 0 )
+	begin
+		raiserror('Código de curso programado no existe.',16,1);
+		return;
+	end;
+	if( @Vacantes = 0 )
+	begin
+		raiserror('Curso no tiene vacantes disponibles.',16,1);
+		return;
+	end;
+	-- Matricula
+	insert into Matricula(IdCursoProg,IdAlumno)
+	values(@IdCursoProg,@IdAlumno);
+	if(@@ERROR <> 0)
+	begin
+		raiserror('Matricula ya se encuentra registrada.',16,1);
+		return;
+	end;
+	-- Actualizar vacantes
+	
+END;
+GO
+
+select * from CursoProgramado where IdCursoProg = 8024;
+select * from Matricula where IdCursoProg = 8024 order by 2;
+go
+
+exec usp_matricula 8024, 'A0001';
+GO
+
+
 
 
 
