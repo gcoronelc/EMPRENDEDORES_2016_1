f0a4a253bcc37fe0004a53052737bc10baaf9395
Gustavo Coronel <gcoronelc@gmail.com>
2016-05-24 14:17:46 -0500

    GAW

--- GAW/Sem11/concultas.sql
+++ GAW/Sem11/concultas.sql
@@ -34,17 +34,54 @@ vch_tipodescripcion
 vch_tipoaccion
 vch_tipoestado
 
+-- Parte 1
+select 
+	m.chr_cuencodigo AS cuenta,
+	tm.vch_tipoaccion,
+	m.dec_moviimporte
+from tipomovimiento tm
+join movimiento m on tm.chr_tipocodigo = m.chr_tipocodigo
+join cuenta c on m.chr_cuencodigo = c.chr_cuencodigo
+where c.chr_sucucodigo = '001';
+
+-- Parte 2
+select 
+	m.chr_cuencodigo AS cuenta,
+	tm.vch_tipoaccion,
+	m.dec_moviimporte,
+	m.dec_moviimporte * case when tm.vch_tipoaccion='SALIDA' 
+		then -1 else 1 end as importe2
+from tipomovimiento tm
+join movimiento m on tm.chr_tipocodigo = m.chr_tipocodigo
+join cuenta c on m.chr_cuencodigo = c.chr_cuencodigo
+where c.chr_sucucodigo = '001';
+
+
+
+
+
+
+
+
+
 select 
 	m.chr_cuencodigo AS cuenta,
 	sum(case when tm.vch_tipoaccion='INGRESO' 
 		then dec_moviimporte else 0 end) as ingreso,
 	sum(case when tm.vch_tipoaccion='SALIDA' 
-		then dec_moviimporte else 0 end) as salida
+		then dec_moviimporte else 0 end) as salida,
+	sum(dec_moviimporte * case when tm.vch_tipoaccion='SALIDA' 
+		then -1 else 1 end) as saldo
 from tipomovimiento tm
 join movimiento m on tm.chr_tipocodigo = m.chr_tipocodigo
 join cuenta c on m.chr_cuencodigo = c.chr_cuencodigo
 where c.chr_sucucodigo = '001'
 group by m.chr_cuencodigo;
+
+
+
+
+
 
 
 
