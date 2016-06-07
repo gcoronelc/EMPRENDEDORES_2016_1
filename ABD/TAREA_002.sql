c4a6b6d495464b5ccb3877540dd28ac94027e20a
Gustavo Coronel <gcoronelc@gmail.com>
2016-06-07 09:28:31 -0500

    ABD Tarea 02

---
+++ ABD/TAREA_002.sql
@@ -0,0 +1,32 @@
+-- BASE DE DATOS: RRHH
+
+/*
+Requerimiento 01
+
+Procedimiento para registrar un nuevo departamento.
+
+*/
+
+
+
+
+/*
+Requerimiento 02
+
+Procedimiento para registrar un nuevo empleado.
+El sueldo debe ser el promedio del sueldo minimo y sueldo maximo segun el cargo.
+El codigo se genera en función a la tabla control.
+La comisión es el 50% del sualdo, pero solo para los vendedores.
+El jefe es el que ocupa el cargo de gerente en el departamento.
+*/
+
+
+/*
+Requerimiento 03
+
+Desarrollar un procedimiento que muestre el importe de la planilla por departamento.
+*/
+
+
+
+
