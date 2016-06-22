4c86f1f8616ba352a4104660c88ba9845c564d20
Gustavo Coronel <gcoronelc@gmail.com>
2016-06-22 15:27:42 -0500

    TBD

--- TBD/TBD_PC4.sql
+++ TBD/TBD_PC4.sql
@@ -16,11 +16,28 @@ Activo int not null check(Activo IN (1,0))
 go
 
 -- Datos de la tabla producto
+insert into Producto(ProductoId,NomProducto,PrecioLista,Stock,Activo)
+values(1000,'Helado de Fresa',5.80,1000,1);
+go
 
+insert into Producto(ProductoId,NomProducto,PrecioLista,Stock,Activo)
+values(2000,'Arros con Leche',7.80,2000,0);
+go
+
+select * from Producto;
+go
 
 
 -- Tabla Venta
 
+
+-- Insertar Ventas
+
+insert into venta(NroVenta,Fecha,Cliente,DirCliente,
+ProductoId,PrecioVenta,Cantidad,Importe)
+values(500,'20160120','Gustavo','Mi Casa',
+1000,8.0,10,80);
+go
 
 
 
