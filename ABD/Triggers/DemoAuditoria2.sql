06c36965e4f11d12e709ab6c52b3e380d85adbb9
Gustavo Coronel <gcoronelc@gmail.com>
2016-06-24 12:35:17 -0500

    ABD

--- ABD/Triggers/DemoAuditoria2.sql
+++ ABD/Triggers/DemoAuditoria2.sql
@@ -37,7 +37,7 @@ CREATE TABLE MATRICULA_AUD(
 GO
 
 -- Trigger de Auditoria
-CREATE TRIGGER dbo.tr_matricula_update_notas_aud
+ALTER TRIGGER dbo.tr_matricula_update_notas_aud
 ON matricula FOR UPDATE
 AS 
 BEGIN
@@ -52,7 +52,14 @@ BEGIN
     @IdCursoProg = IdCursoProg, @IdAlumno = IdAlumno,
     @aud_fecha = aud_fecha, @aud_usuario = aud_usuario,
     @aud_host = aud_host
-  from deleted;
+  from inserted;
+
+  if(@aud_fecha is null)
+    set @aud_fecha = GETDATE();
+  if(@aud_usuario is null)
+    set @aud_usuario = USER_NAME();
+  if(@aud_host is null)
+    set @aud_host = HOST_NAME();
 
   -- Verificando ExaParcial
   if( UPDATE (ExaParcial) )
@@ -71,8 +78,75 @@ BEGIN
     end;
   end;
 
+  -- Verificando ExaFinal
+  if( UPDATE (ExaFinal) )
+  begin
+    select @nota_old = ExaFinal from deleted;
+    select @nota_new = ExaFinal from inserted;
+    if(@nota_old != @nota_new)
+    begin
+      insert into MATRICULA_AUD(IdCursoProg,IdAlumno,fecha,accion,
+        columna,valor_old,valor_new,login,user_sql,user_so,host,
+        aud_fecha,aud_usuario,aud_host)
+      values(@IdCursoProg,@IdAlumno,GETDATE(),'UPDATE','ExaFinal',
+        cast(@nota_old as varchar), cast(@nota_new as varchar),
+        SUSER_NAME(),USER_NAME(),NULL,HOST_NAME(),
+        @aud_fecha,@aud_usuario,@aud_host);
+    end;
+  end;
+
+  -- Verificando Promedio
+  if( UPDATE (Promedio) )
+  begin
+    select @nota_old = Promedio from deleted;
+    select @nota_new = Promedio from inserted;
+    if(@nota_old != @nota_new)
+    begin
+      insert into MATRICULA_AUD(IdCursoProg,IdAlumno,fecha,accion,
+        columna,valor_old,valor_new,login,user_sql,user_so,host,
+        aud_fecha,aud_usuario,aud_host)
+      values(@IdCursoProg,@IdAlumno,GETDATE(),'UPDATE','Promedio',
+        cast(@nota_old as varchar), cast(@nota_new as varchar),
+        SUSER_NAME(),USER_NAME(),NULL,HOST_NAME(),
+        @aud_fecha,@aud_usuario,@aud_host);
+    end;
+  end;
+
+  -- Verificando ExaSub
+  if( UPDATE (ExaSub) )
+  begin
+    select @nota_old = Promedio from deleted;
+    select @nota_new = Promedio from inserted;
+    if(@nota_old != @nota_new)
+    begin
+      insert into MATRICULA_AUD(IdCursoProg,IdAlumno,fecha,accion,
+        columna,valor_old,valor_new,login,user_sql,user_so,host,
+        aud_fecha,aud_usuario,aud_host)
+      values(@IdCursoProg,@IdAlumno,GETDATE(),'UPDATE','ExaSub',
+        cast(@nota_old as varchar), cast(@nota_new as varchar),
+        SUSER_NAME(),USER_NAME(),NULL,HOST_NAME(),
+        @aud_fecha,@aud_usuario,@aud_host);
+    end;
+  end;
+
 END;
 GO
+
+SELECT * FROM MATRICULA_AUD order by 1 desc;
+GO
+
+SELECT * FROM MATRICULA ORDER BY 1 DESC;
+
+UPDATE MATRICULA
+SET 
+  ExaParcial = 13, ExaFinal = 14, 
+  Promedio = (13 + 14) / 2, aud_usuario='Hugito'
+  --,aud_fecha = '20160615'
+where IdCursoProg = 8348 and IdAlumno = 'A0049';
+go
+
+
+
 
 
 
