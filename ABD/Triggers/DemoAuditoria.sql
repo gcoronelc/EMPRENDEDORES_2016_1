f6a82a611d359bc95e9eeb7ad3cd19e4a4842557
Gustavo Coronel <gcoronelc@gmail.com>
2016-06-24 11:02:55 -0500

    ABD

--- ABD/Triggers/DemoAuditoria.sql
+++ ABD/Triggers/DemoAuditoria.sql
@@ -1,9 +1,9 @@
--- CREACIÓN DE UN LOG
+-- CREACIÓN UNA TABLA DE AUDITORIA PARA CURSO
 
-CREATE TABLE AUDITORIA(
+CREATE TABLE CURSO_AUD(
   id int not null identity primary key,
+  cur_id int not null,
   fecha   date not null,
-  tabla varchar(100) not null,
   accion varchar(30) not null,
   columna varchar(50) not null,
   valor_old varchar(200) not null,
@@ -15,21 +15,20 @@ CREATE TABLE AUDITORIA(
 );
 GO
 
-alter TRIGGER dbo.tr_curso_update_precio_log
-ON curso 
-FOR UPDATE
+CREATE TRIGGER dbo.tr_curso_update_precio_aud
+ON curso FOR UPDATE
 AS 
 BEGIN
 
-  declare @precio_old money, @precio_new money;
+  declare @precio_old money, @precio_new money, @cur_id int;
 
   if( UPDATE (cur_precio) )
   begin
-    select @precio_old = cur_precio from deleted;
+    select @cur_id = cur_id, @precio_old = cur_precio from deleted;
     select @precio_new = cur_precio from inserted;
-    insert into AUDITORIA(fecha,tabla,accion,columna,valor_old,
+    insert into CURSO_AUD(cur_id,fecha,accion,columna,valor_old,
       valor_new,login,user_sql,user_so,host)
-    values(GETDATE(),'CURSO','UPDATE','cur_precio',
+    values(@cur_id,GETDATE(),'UPDATE','cur_precio',
       cast(@precio_old as varchar), cast(@precio_new as varchar),
       SUSER_NAME(),USER_NAME(),NULL,HOST_NAME());
   end;
@@ -37,10 +36,14 @@ BEGIN
 END;
 GO
 
-update curso set cur_precio = 1350.0 where cur_id = 1;
+update curso set cur_precio = 1400.0 where cur_id = 1;
 go
 
-select * from auditoria;
+update curso set cur_precio = 1600.0 where cur_id = 3;
+go
+
+select * from curso;
+select * from CURSO_AUD order by 1 desc;
 go
 
 select SUSER_NAME();
