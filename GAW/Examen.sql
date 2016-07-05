e4af30f47151e2018a66b86d59070bfdd73e8a16
Gustavo Coronel <gcoronelc@gmail.com>
2016-07-05 13:47:56 -0500

    GAW

--- GAW/Examen.sql
+++ GAW/Examen.sql
@@ -5,3 +5,13 @@ insert into asignado(chr_asigcodigo,chr_sucucodigo,
 chr_emplcodigo,dtt_asigfechaalta)
 values('000012','004','0008',DATE_ADD(sysdate(), INTERVAL 1 DAY));
 
+select vch_monedescripcion MONEDA,
+sum(case when tm.vch_tipoaccion = 'INGRESO' 
+         then mov.dec_moviimporte else 0 end) INGRESO,
+sum(case when tm.vch_tipoaccion = 'SALIDA' 
+         then mov.dec_moviimporte else 0 end) SALIDA		 
+from moneda mon
+join cuenta c on mon.chr_monecodigo = c.chr_monecodigo
+join movimiento mov on c.chr_cuencodigo = mov.chr_cuencodigo
+join tipomovimiento tm on mov.chr_tipocodigo = tm.chr_tipocodigo
+group by mon.vch_monedescripcion;
